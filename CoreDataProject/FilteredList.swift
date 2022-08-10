d0856c83eb9b7a616420a1863f368c3e28a0d9a5
Landon Cayia <landon.cayia@gmail.com>
2022-08-10 15:00:14 -0400

    Create FilteredList view, show in main view

---
+++ CoreDataProject/FilteredList.swift
@@ -0,0 +1,22 @@
+//
+//  FilteredList.swift
+//  CoreDataProject
+//
+//  Created by Landon Cayia on 8/10/22.
+//
+
+import SwiftUI
+
+struct FilteredList: View {
+    @FetchRequest var fetchRequest: FetchedResults<Singer>
+    
+    var body: some View {
+        List(fetchRequest, id: \.self) { singer in
+            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+        }
+    }
+    
+    init(filter: String) {
+        _fetchRequest = FetchRequest<Singer>(sortDescriptors: [], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter))
+    }
+}
