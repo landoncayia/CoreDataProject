6e7fade5ec475a59780dac77b216378b42929b60
Landon Cayia <landon.cayia@gmail.com>
2022-08-10 14:51:25 -0400

    Add wrapped versions of Singer properties

--- Singer+CoreDataProperties.swift
+++ Singer+CoreDataProperties.swift
@@ -18,7 +18,14 @@ extension Singer {
 
     @NSManaged public var lastName: String?
     @NSManaged public var firstName: String?
-
+    
+    var wrappedFirstName: String {
+        firstName ?? "Unknown"
+    }
+    
+    var wrappedLastName: String {
+        lastName ?? "Unknown"
+    }
 }
 
 extension Singer : Identifiable {
